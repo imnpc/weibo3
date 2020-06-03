05bf1a6e06e63590bf9fb5106a8f83b836958b9f
“imnpc” <qilidi@gmail.com>
2020-06-03 08:38:51 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -18,18 +19,21 @@ class UsersController extends Controller
     }
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'name'=>'required|unique:users|max:50',
-            'email'=>'required|email|unique:users|max:255',
-            'password'=>'required|confirmed|min:6'
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
         ]);
+
         $user = User::create([
-           'name'=>$request->name,
-           'email'=>$request->email,
-           'password'=>bcrypt($request->password),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程');
-        return redirect()->route('users.show',[$user]);
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
     public function store1(Request $request)
     {
